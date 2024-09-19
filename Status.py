40a215028dfbef2fa0d9ba02f8781c94f145bd45
Josh Hall <lostprophet217@gmail.com>
2024-09-19 14:18:20 -0500

    Updated with docs and suggested refactoring

--- Status.py
+++ Status.py
@@ -7,15 +7,14 @@ class Status(Enum):
     IN_PROGRESS = "In Progress"
 
     @classmethod
-    def _init_status_map(cls):
-        cls.STATUS_MAP = {status.value: status for status in Status}
+    def initialize_status_map(cls):
+        if not hasattr(cls, 'STATUS_MAP'):
+            cls.STATUS_MAP = {status.value: status for status in Status}
 
     @classmethod
     def from_string(cls, status_string: str) -> 'Status':
+        cls.initialize_status_map()
         try:
             return cls.STATUS_MAP[status_string]
         except KeyError:
             raise ValueError(f"'{status_string}' not a valid Status")
-
-
-Status._init_status_map()
--- Task.py
+++ Task.py
@@ -3,25 +3,30 @@ from typing import Union
 
 from Status import Status
 
+DEFAULT_STATUS = Status.NOT_COMPLETED
+
 
 @dataclass
 class Task:
     description: str
     status: Status
 
-    def __init__(self, description: str, status: Union[str, Status] = Status.NOT_COMPLETED):
+    def __init__(self, description: str, status: Union[str, Status] = DEFAULT_STATUS):
         self.description = description
+        self.status = self._convert_status(status)
+
+    def _convert_status(self, status: Union[str, Status]) -> Status:
         if isinstance(status, str):
-            self.status = Status.from_string(status)
+            return Status.from_string(status)
         elif isinstance(status, Status):
-            self.status = status
+            return status
         else:
             raise TypeError("status must be either a str or a Status instance")
 
     @classmethod
-    def from_strings(cls, description: str, status: str = Status.NOT_COMPLETED.value) -> 'Task':
+    def from_string(cls, description: str, status: str = DEFAULT_STATUS.value) -> 'Task':
         return cls(description, status)
 
     @classmethod
-    def from_status(cls, description: str, status: Status = Status.NOT_COMPLETED) -> 'Task':
+    def from_status(cls, description: str, status: Status = DEFAULT_STATUS) -> 'Task':
         return cls(description, status)
